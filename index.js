6b31282dc44eb88bf9627cdf0490b805f0470bff
IT20273408 <it20273408@my.sliit.lk>
2022-01-01 12:01:15 +0530

    Type of Function

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+//performing a task
 function greet(name, lastName) {
     console.log('Hello' + name + '' +  lastName );
 }
+//Calculate a value
+function square(number) {
+    return number * number;
 
-greet(' Jaya', ' Smith ');
+
+}
+
+console.log(square(2));
