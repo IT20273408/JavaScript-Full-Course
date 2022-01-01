792209489de33cb6ff6343ae1987d385fca3e6c6
IT20273408 <it20273408@my.sliit.lk>
2022-01-01 19:49:33 +0530

    Equality Operators

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-let x = 1;
+//Strict Equality (Type+Value)
+// console.log(1 ===1);
+// console.log('1' === 1);
 
-console.log(x> 0);
-console.log(x >=1);
-console.log(x<1);
-console.log(x <=1);
+//Lose Equality
+console.log(1 ==1);
+console.log('1' == 1);
+console.log(true == 1);
 
-//Equality
-console.log(x === 1);
-console.log(x !==1);
+
+
+
 
