57ec70771d8affe42996a9792da6148df0c7642a
IT20273408 <it20273408@my.sliit.lk>
2022-01-07 22:44:46 +0530

    if..else

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let a = 'red';
-let b ='blue';
+//Hour 
+//If hour is 6am - 12pm - gm
+//if it is 12pm - 6am -ga
+//otherwise ge
 
-let c = a;
-a = b;
-b = c;
+let hour =13;
 
-console.log(a);
-console.log(b);
+if (hour >= 6 && hour < 12) 
+    console.log('GM');
+
+else if (hour >= 12 && hour < 18) 
+    console.log('GA');
+
+ else 
+ console.log('GE');
