2e226c6b180c3fb0cd73b9d5e117efe8743fc4b0
IT20273408 <it20273408@my.sliit.lk>
2022-01-07 22:51:11 +0530

    switch..case

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-//Hour 
-//If hour is 6am - 12pm - gm
-//if it is 12pm - 6am -ga
-//otherwise ge
+let role = 'guest' ;
 
-let hour =13;
+switch (role) {
+    case 'guest': 
+    console.log('Guest User');
+    break
 
-if (hour >= 6 && hour < 12) 
-    console.log('GM');
+    case 'moderator':
+        console.log('Moderator User');
+        break
 
-else if (hour >= 12 && hour < 18) 
-    console.log('GA');
+        default:
+        console.log('Unknown User');
+        
 
- else 
- console.log('GE');
+}
