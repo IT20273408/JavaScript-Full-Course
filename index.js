8dd53fbb643a4126a93217ec175b9855c89199c9
Jayathmi Hennayake <88605573+JayaHennayake@users.noreply.github.com>
2022-05-13 22:22:17 +0530

    js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-let number = max (3, 10);
+/*let number = max (3, 10);
 console.log(number);
 
 function max(a, b) {
 
      return (a> b) ? a: b;
-}
+} */
+
+var firstName = "Jay";
+var lastName = "Perera";
+var age = 26;
+var isOk = true;
+var job, height ;
+
+console.log(firstName);
+console.log('My name is'+lastName);
+
+console.log('My age is'+age);
+console.log('It is'+isOk);
+console.log('I am a '+job);
+
+job = 'Doctor';
+lastName = 'Fernando';
+console.log('I am ' + lastName+ 'I am a'+job);
+
+alert('My name is ' + lastName+ '. I am ' + age+ 'years old');
+
+height = prompt('What is your height?');
+alert('My height is'+ height + 'feet.');
